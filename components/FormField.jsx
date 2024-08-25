64ac9dd8a5101a1f7cd0da3e1f30156fe98259b0
Milton3000 <kristofferssonmilton@gmail.com>
2024-08-25 21:15:48 +0200

    TextInput Fields added + security for password

--- components/FormField.jsx
+++ components/FormField.jsx
@@ -1,7 +1,9 @@
 import { View, Text, TextInput } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 
 const FormField = ({ title, value, placeholder, handleChangeText, otherStyles, ...props }) => {
+
+    const [showPassword, setShowPassword] = useState(false)
   return (
     <View className={`space-y-2 ${otherStyles}`}>
       <Text className="text-base text-gray-100 font-pmedium">{title}</Text>
@@ -9,7 +11,13 @@ const FormField = ({ title, value, placeholder, handleChangeText, otherStyles, .
       <View className=" border-2 border-black-200 w-full h-16 px-4 bg-black-100 rounded-2xl focus:border-secondary items-center">
 
 <TextInput
-className=""
+className="flex-1 text-white font-psemibold text-base"
+value={value}
+placeholder={placeholder}
+placeholderTextColor="#7b7b8b"
+onChangeText={handleChangeText}
+secureTextEntry={title === 'Password' && !showPassword}
+// SecureTextEntry = if it is a password, we want to hide it. 
 />
       </View>
     </View>
