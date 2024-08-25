ac7e601eff5b08a320e29f97d482286f1170adb8
Milton3000 <kristofferssonmilton@gmail.com>
2024-08-25 21:28:50 +0200

    CSS (-top)

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -15,14 +15,14 @@ const SignIn = () => {
   return (
 <SafeAreaView className="bg-primary h-full">
 <ScrollView>
-  <View className="w-full justify-center h-full px-4">
+  <View className="w-full justify-center h-full px-4 -top-16">
     {/* my-6 and/or min-h-[80vh] for above */}
 <Image 
 source={images.bettylogo4}
 resizeMode="contain"
 className="w-[155px] h-[155px]" />
 
-<Text className="text-2xl text-white text-semibold mt-5 font-psemibold">
+<Text className="text-2xl text-white text-semibold mt-5 font-psemibold -top-10">
   Log in to Betty
 </Text>
 <FormField
