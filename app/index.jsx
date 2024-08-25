05a15a807a0c042e96aa3c70312b03fec76f114a
Milton3000 <kristofferssonmilton@gmail.com>
2024-08-25 18:26:58 +0200

    Images + Font + Title

--- app/index.jsx
+++ app/index.jsx
@@ -11,20 +11,19 @@ export default function App() {
 <ScrollView contentContainerStyle={{ height: '100%'}}>
 <View className="w-full justify-center items-center h-full px-4">
 <Image 
-source={images.bettylogo}
-className="w-[130px] h-[84px]"
-resizeMode="contain"
+source={images.bettylogo3}
+className="w-[200px] h-[200px]"
 />
 
 <Image
-source={images.cards}
-className="max-w-[380px] w-full h-[300px]"
+source={images.betty2}
+className="max-w-[380px] w-full h-[300px] -top-9"
 resizeMode='contain'
 />
 
-<View className="relative mt-5">
+<View className="relative mt-5 -top-5">
 <Text className="text-3xl text-white font-bold text-center">
-Discover Endless Possibilities with {' '}
+Discover Endless Sharing with {' '}
 <Text className="text-secondary-200">
     Betty
 </Text>
@@ -36,9 +35,9 @@ resizeMode='contain'
 />
 </View>
 
-<Text className="text-sm font-pregular text-gray-100 mt-7 text-center">
+{/* <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">
     Where creativity meets innovation: embark on a journey of limitless exploration with Betty
-</Text>
+</Text> */}
 
 </View>
 </ScrollView>
--- constants/images.js
+++ constants/images.js
@@ -5,5 +5,9 @@ import path from "../assets/images/path.png";
 import logo from "../assets/images/logo.png";
 import logoSmall from "../assets/images/logo-small.png";
 import empty from "../assets/images/empty.png";
+import bettylogo2 from "../assets/images/bettylogo2.png"; // Roboto
+import bettylogo3 from "../assets/images/bettylogo3.png"; // Poppins
+import betty1 from "../assets/images/betty1.webp";
+import betty2 from "../assets/images/betty2.webp";
 
-export default { profile, thumbnail, cards, path, logo, logoSmall, empty };
+export default { profile, thumbnail, cards, path, logo, logoSmall, empty, bettylogo2, bettylogo3, betty1, betty2 };
