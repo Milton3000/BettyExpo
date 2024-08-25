bdc03001dec747a077cf9f048886161ec2f7310f
Milton3000 <kristofferssonmilton@gmail.com>
2024-08-25 21:38:30 +0200

    Sign up button /redirect added

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -4,6 +4,8 @@ import { SafeAreaView } from 'react-native-safe-area-context'
 
 import { images } from "../../constants";
 import FormField from '../../components/FormField';
+import CustomButton from '../../components/CustomButton';
+import { Link } from 'expo-router';
 
 const SignIn = () => {
 
@@ -11,6 +13,12 @@ const SignIn = () => {
     email: '',
     password: ''
   })
+
+  const [isSubmitting, setIsSubmitting] = useState(false)
+
+  const submit = () => {
+
+  }
 
   return (
 <SafeAreaView className="bg-primary h-full">
@@ -38,6 +46,22 @@ value={form.password}
 handleChangeText={(e) => setForm({ ...form, password: e })}
 otherStyles="mt-7"
 />
+
+<CustomButton
+title="Sign In"
+handlePress={submit}
+containerStyles="mt-7"
+isLoading={isSubmitting}
+/>
+<View className="justify-center pt-5 flex-row gap-2">
+<Text className="text-lg text-gray-100 font-pregular">
+  Don't have an account?
+</Text>
+<Link href="/sign-up" className='text-lg font-psemibold text-secondary'>
+Sign Up
+</Link>
+
+</View>
   </View>
 </ScrollView>
 </SafeAreaView>
