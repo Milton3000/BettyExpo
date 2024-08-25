681e42114831743a957c2a9efcfffc83e6b5f974
Milton3000 <kristofferssonmilton@gmail.com>
2024-08-25 23:07:43 +0200

    Appwrite SDK + Create a User

--- app/(auth)/sign-up.jsx
+++ app/(auth)/sign-up.jsx
@@ -6,6 +6,7 @@ import { images } from "../../constants";
 import FormField from '../../components/FormField';
 import CustomButton from '../../components/CustomButton';
 import { Link } from 'expo-router';
+import { createUser } from '../../lib/appwrite';
 
 const SignUp = () => {
 
@@ -18,7 +19,7 @@ const SignUp = () => {
   const [isSubmitting, setIsSubmitting] = useState(false)
 
   const submit = () => {
-
+    createUser();
   }
 
   return (
@@ -55,7 +56,7 @@ otherStyles="mt-7"
 />
 
 <CustomButton
-title="Sign In"
+title="Sign Up"
 handlePress={submit}
 containerStyles="mt-7"
 isLoading={isSubmitting}
--- app/index.jsx
+++ app/index.jsx
@@ -2,9 +2,9 @@ import { StatusBar } from 'expo-status-bar';
 import { Image, ScrollView, Text, View } from 'react-native';
 import { Redirect, router } from "expo-router";
 import { SafeAreaView } from 'react-native-safe-area-context';
-
 import { images } from "../constants";
 import CustomButton from '../components/CustomButton';
+// import 'react-native-url-polyfill/auto';
 
 export default function App() {
     return (
