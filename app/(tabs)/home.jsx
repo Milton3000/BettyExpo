0c37c7072bd3d430d267d4b85e494682de44cce7
Milton3000 <kristofferssonmilton@gmail.com>
2024-08-27 13:24:05 +0200

    Home + css fix

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -5,19 +5,20 @@ import { SafeAreaView } from 'react-native-safe-area-context'
 import { images } from "../../constants";
 import SearchInput from '../../components/SearchInput';
 import Trending from '../../components/Trending';
+import EmptyState from '../../components/EmptyState';
 
 const Home = () => {
   return (
-    <SafeAreaView className="bg-primary">
+    <SafeAreaView className="bg-primary h-full">
       <FlatList
-        data={[{ id: 1 }, { id: 2 }, { id: 3 },]}
-        keyExtractor={(item) => item.$id}
+        // data={[{ id: 1 }, { id: 2 }, { id: 3 },]}
+        // keyExtractor={(item) => item.$id}
         renderItem={({ item }) => (
           <Text className="text-3xl text-white"> {item.id}
           </Text>
         )}
         ListHeaderComponent={() => (
-          <View className="my-6 px-4 space-y-6">
+          <View className="my-6 px-4 space-y-1">
             <View className="justify-between items-start flew-row mb-6">
               <View>
                 <Text className="font-pmedium text-sm text-gray-100">
@@ -36,7 +37,7 @@ const Home = () => {
               </View>
             </View>
             <SearchInput />
-            <View className="w-full flex-1 pt-5 pb-8">
+            <View className="w-full flex-1 pt-5 pb-5">
           <Text className="text-gray-100 text-lg font-pregular mb-3">
             Latest Videos
           </Text>
@@ -45,7 +46,10 @@ const Home = () => {
           </View>
         )}
         ListEmptyComponent={() => (
-          <Text className="text-white">Empty</Text>
+        <EmptyState
+        title="No images found"
+        subtitle="Be the first one to upload an image"
+        />
         )}
       />
     </SafeAreaView>
