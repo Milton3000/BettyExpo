33c5c59516519afa379a2164825cc7bb1c71f575
Milton3000 <kristofferssonmilton@gmail.com>
2024-09-05 12:12:20 +0200

    Fix

--- app/(tabs)/profile.jsx
+++ app/(tabs)/profile.jsx
@@ -1,4 +1,4 @@
-import { View, Text, FlatList, TouchableOpacity, Image, } from 'react-native'
+import { View, FlatList, TouchableOpacity, Image, } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import ImageCard from '../../components/ImageCard'; // ImageCard
 import VideoCard from '../../components/VideoCard';
@@ -7,6 +7,7 @@ import { videoCollectionId, imageCollectionId, getUserPosts } from '../../lib/ap
 import useAppwrite from '../../lib/useAppwrite';
 import { useGlobalContext } from '../../context/GlobalProvider';
 import { icons } from '../../constants';
+import InfoBox from '../../components/InfoBox';
 
 const Profile = () => {
   const { user } = useGlobalContext();
@@ -31,16 +32,34 @@ const Profile = () => {
         ListHeaderComponent={() => (
           <View className="w-full justify-center items-center mt-6 mb-12 px-4">
             <TouchableOpacity className="w-full items-end mb-10"
-            onPress={logout}
+              onPress={logout}
             >
-          <Image 
-          source={icons.logout}
-          resizeMode='contain'
-          className="w-6 h-6"
-          />
+              <Image
+                source={icons.logout}
+                resizeMode='contain'
+                className="w-6 h-6"
+              />
             </TouchableOpacity>
             <View className="w-16 h-16 border border-secondary rounded-lg justify-center items-center">
-          <Image source={{ uri: user?.avatar}} className="w-[90%] h-[90%] rounded-lg" resizeMode='cover'/>
+              <Image source={{ uri: user?.avatar }} className="w-[90%] h-[90%] rounded-lg" resizeMode='cover' />
+            </View>
+            <InfoBox
+              title={user?.username}
+              containerStyles="mt-5"
+              titleStyles="text-lg"
+            />
+            <View className="mt-5 flex-row">
+              <InfoBox
+                title={posts.length || 0}
+                subtitle="Posts"
+                containerStyles="mr-10"
+                titleStyles="text-xl"
+              />
+              <InfoBox
+                title="1.2k" // KAN SKIPPA FOLLOWERS SEN
+                subtitle="Followers"
+                titleStyles="text-xl"
+              />
             </View>
           </View>
         )}
